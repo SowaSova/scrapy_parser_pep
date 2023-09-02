0af175725c3eadef877ee913196af3d64886b23d
SowaSova <cei4bio@gmail.com>
2023-09-02 11:52:04 +0600

    1st

---
+++ pep_parse/pipelines.py
@@ -0,0 +1,35 @@
+import csv
+from datetime import datetime as dt
+
+from itemadapter import ItemAdapter
+
+from pep_parse.settings import BASE_DIR
+
+
+class PepParsePipeline:
+    def open_spider(self, spider):
+        self.status_count = {}
+        self.total_peps = 0
+
+    def process_item(self, item, spider):
+        status = item["status"]
+
+        if status:
+            self.status_count[status] = self.status_count.get(status, 0) + 1
+
+        self.total_peps += 1
+
+        return item
+
+    def close_spider(self, spider):
+        now = dt.now().strftime("%Y-%m-%d_%H-%M-%S")
+        file_dir = BASE_DIR / "results"
+        file_dir.mkdir(exist_ok=True)
+        filename = f"status_summary_{now}.csv"
+
+        with open(file_dir / filename, "w", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Статус", "Количество"])
+            for status, count in self.status_count.items():
+                writer.writerow([status, count])
+            writer.writerow(["TOTAL", self.total_peps])
---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,32 @@
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = "pep"
+    allowed_domains = ["peps.python.org"]
+    start_urls = ["https://peps.python.org/"]
+
+    def parse(self, response):
+        table = response.css("section#numerical-index")
+        for res in table.css("tbody > tr"):
+            url = res.css("td:nth-child(2) a::attr(href)").get()
+            meta_data = {
+                "number": res.css("td:nth-child(2) a::text").get(),
+                "name": res.css("td:nth-child(3) a::text").get(),
+            }
+            yield response.follow(url, callback=self.parse_pep, meta=meta_data)
+
+    def parse_pep(self, response):
+        status = (
+            response.css("dl.rfc2822.field-list.simple")
+            .xpath("dt[contains(., 'Status:')]/following::*/text()")
+            .get()
+        )
+        data = {
+            "number": response.meta["number"],
+            "name": response.meta["name"],
+            "status": status,
+        }
+        yield PepParseItem(data)
